71530384d1ece8198aa7bc677a4a461bc9337c90
Michael Hodson <hm150164@tmc.catholic.edu.au>
2018-09-10 16:23:16 +0930

    My3DGame
    MyFirst3DGame

---
+++ Assets/Scripts/PlayerCollison.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerCollison : MonoBehaviour{
+
+    public PlayerMovement movement; //A reference to our PlayerMovement script
+    
+    // This function runs when we hit another object.
+    // We get information about the collision and call it "collisionInfo".
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        // We check if the object we collided with a tag called "Obstcle".
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false; // Disable the Players movement.
+            FindObjectOfType<GameManager>().EndGame();
+            
+        }
+       
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // This is a reference to the Ridgidbody componet called "rb"
+    public Rigidbody rb;
+
+    public float fowardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+
+	// We marked this as "Fixed"Update becuase we
+    // are using it to mess with physics.
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        // Add a forward force
+        rb.AddForce(0, 0, fowardForce * Time.deltaTime);
+
+        if ( Input.GetKey ("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
